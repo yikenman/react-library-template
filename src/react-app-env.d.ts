0253010cd94920ef2dc9652db421570905a9710e
yikenman <yikenman@gmail.com>
2019-08-29 10:54:24 +0800

    update style type definition

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,26 +1,11 @@
 /// <reference types="react-scripts" />
 
-declare module '*.module.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-declare module '*.sass' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
 declare module '*.less' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.less' {
   const classes: { [key: string]: string };
   export default classes;
 }
